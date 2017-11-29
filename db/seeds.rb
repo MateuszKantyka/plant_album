df48abed7b7b231c1ec8191367dd937a650bb8f2
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2017-11-29 22:15:26 +0100

    Adding seed to Users and secure password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!( name: 'Mateusz Kantyka',
+              email: 'mateusz.kantyka@hotmail.com',
+              password: '1234',
+              password_confirmation: '1234',
+              admin: true)
+
+User.create!( name: 'Justyna Kawa',
+              email: 'juskaw123@onet.pl',
+              password: '1111',
+              password_confirmation: '1111',
+              admin: false)
