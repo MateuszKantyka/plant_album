5968860e76fe586f50bc9d5a2af5b36b3e96bf8e
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2017-12-02 16:41:18 +0100

    Adding login and logout mechanize

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      flash.now[:success] = "Correct mail password"
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
