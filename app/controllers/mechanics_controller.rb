c4a1d5d5cd45227fa6dd3efdef9a1e9e588c3ce5
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2017-12-05 00:50:01 +0100

    First step to show comments in mechanic site

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,5 +6,7 @@ class MechanicsController < ApplicationController
 
   def show
     @mechanic = Mechanic.find(params[:id])
+    #@comment = Comment.find_by(mechanic_id: 1)
+    @comment = Comment.find(:all, :conditions => { :mechanic_id => 1 })
   end
 end
