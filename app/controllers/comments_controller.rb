2cf8cd28e78767a17dbdb0df527b71e473e712e7
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2017-12-07 23:37:57 +0100

    Comments action - create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,15 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     if @comment.save
       flash[:success] = "User successfully created"
+      redirect_to request.referrer
     else
-      redirect_to root_url
+      redirect_to request.referrer
     end
   end
 
   def comment_params
     params.require(:comment).permit(:content,:car,:cost, :rating_general,
-                                 :rating_cost, :rating_time)
+                                 :rating_cost, :rating_time, :mechanic_id)
+                                 .merge(user_id: current_user.id)
   end
-
 end
