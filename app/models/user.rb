b0921d912789dab4cf5bf0cd80746e6707745423
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2017-12-11 23:32:37 +0100

    adding validates to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  before_save :downcase_email
   has_secure_password
   has_many :comments, dependent: :delete_all
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: { minimum: 8 }
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
