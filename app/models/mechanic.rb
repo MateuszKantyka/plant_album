446aa738b67ae1fb197bce23a34c40d9bdc747d4
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2017-12-12 00:09:52 +0100

    adding validates to mechanics model

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,3 +1,11 @@
 class Mechanic < ApplicationRecord
   has_many :comments
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :city, presence: true, length: { maximum: 48 }
+  validates :post_code, presence: true, length: { minimum: 48 }
+  validates :address, presence: true, length: { minimum: 48 }
+  validates :rating, presence: true, length: { minimum: 5 }
+  validates :phone_number, presence: true, length: { minimum: 15 }
+  validates :description, presence: true, length: { maximum: 255 }
 end
