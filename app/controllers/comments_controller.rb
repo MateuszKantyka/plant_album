3b0579ea7baa85039e9e69dbcf876c53a5b0902e
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2017-12-13 00:07:27 +0100

    Adding refresh mechanic rating method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
+      refresh_rating(@comment)
       flash[:success] = "Comment successfully created"
       redirect_to request.referrer
     else
@@ -13,6 +14,7 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment = Comment.find(params[:id])
+    refresh_rating(@comment)
     @comment.destroy
     flash[:success] = "Comment successfully destroyed"
     redirect_to request.referrer
@@ -22,5 +24,16 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:content,:car,:cost, :rating_general,
                                  :rating_cost, :rating_time, :mechanic_id)
                                  .merge(user_id: current_user.id)
+  end
+
+  def refresh_rating(comment)
+    mechanic = Mechanic.find(comment.mechanic_id)
+    rating = 0.0
+    i = 0
+    mechanic.comments.each do |comment|
+      rating = rating + ((comment.rating_cost + comment.rating_time + comment.rating_general) / 3)
+      i = i+1
+    end
+    mechanic.update_attribute(:rating,rating / i)
   end
 end
