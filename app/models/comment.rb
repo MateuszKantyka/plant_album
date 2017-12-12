27d0d4531ed244683d4e7b55270e2d784f044298
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2017-12-12 19:14:24 +0100

    fix validates for comment and mechanic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,8 @@ class Comment < ApplicationRecord
 
   validates :content, presence: true, length: { maximum: 255 }
   validates :car, presence: true, length: { maximum: 100 }
-  validates :cost, presence: true, length: { minimum: 24 }
-  validates :rating_general, presence: true, length: { minimum: 5 }
-  validates :rating_cost, presence: true, length: { minimum: 5 }
-  validates :rating_time, presence: true, length: { minimum: 5 }
+  validates :cost, presence: true, length: { maximum: 24 }
+  validates :rating_general, presence: true, inclusion: 1..5
+  validates :rating_cost, presence: true, inclusion: 1..5
+  validates :rating_time, presence: true, inclusion: 1..5
 end
