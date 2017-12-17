79cbbefdb55c4b279b7088e125554362f72994e7
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2017-12-17 22:24:30 +0100

    Change users password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 User.create!( name: 'Mateusz Kantyka',
               email: 'mateusz.kantyka@hotmail.com',
-              password: '1234',
-              password_confirmation: '1234',
+              password: '12345678',
+              password_confirmation: '12345678',
               admin: true)
 
 User.create!( name: 'Justyna Kawa',
               email: 'juskaw123@onet.pl',
-              password: '1111',
-              password_confirmation: '1111',
+              password: '12345678',
+              password_confirmation: '12345678',
               admin: false)
 
 Mechanic.create!( name: "Nowak autoMechanic s.c.",
