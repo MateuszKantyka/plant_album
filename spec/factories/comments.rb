d9a82d1390f7fc89fd42f63a9ec363094db5ad02
fuger <mateusz.kantyka@hotmail.com>
2017-12-20 23:28:01 +0100

    Adding new test for delete comment

---
+++ spec/factories/comments.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :comment do
+    content 'example comment'
+    car 'example car'
+    cost "100"
+    rating_general 5
+    rating_cost 5
+    rating_time 5
+  end
+end
--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -9,7 +9,6 @@ RSpec.feature 'Comment' do
     allow_any_instance_of(SessionsHelper).to receive(:current_user) { user }
 
     visit 'mechanics/1'
-
     expect(page).not_to have_content 'Delete'
 
     fill_in :comment_content, with: 'My comment'
@@ -30,7 +29,6 @@ RSpec.feature 'Comment' do
     allow_any_instance_of(SessionsHelper).to receive(:current_user) { user }
 
     visit 'mechanics/1'
-
     expect(page).not_to have_content 'Delete'
 
     fill_in :comment_content, with: 'My comment'
@@ -42,19 +40,45 @@ RSpec.feature 'Comment' do
 
   scenario "user can't delete other users comments" do
     user = create(:user)
-    create(:mechanic)
+    user2 = create(:user, email: 'example@wp.pl')
+    mechanic = create(:mechanic)
+    create(:comment, mechanic: mechanic, user: user2)
 
     allow_any_instance_of(SessionsHelper).to receive(:current_user) { user }
+    visit 'mechanics/1'
 
-    visit root_path
-    click_on 'More info'
-  end
-
-  scenario "admin can delete all comments" do
-
+    expect(page).to have_content 'example comment'
+    expect(page).not_to have_content 'Delete'
   end
 
   scenario "user can delete only his own comments" do
+    user = create(:user)
+    user2 = create(:user, email: 'example@wp.pl')
+    mechanic = create(:mechanic)
+    create(:comment, mechanic: mechanic, user: user)
+    create(:comment, mechanic: mechanic, user: user2)
 
+    allow_any_instance_of(SessionsHelper).to receive(:current_user) { user }
+    visit 'mechanics/1'
+
+    expect(page).to have_content('example comment', count: 2)
+    expect(page).to have_selector(:link_or_button, 'Delete', count: 1)
+    click_on 'Delete'
+    expect(page).to have_content 'Comment successfully destroyed'
+  end
+
+  scenario "admin can delete all comments" do
+    admin = create(:user, admin: true)
+    user = create(:user, email: 'example@mail.com')
+    mechanic = create(:mechanic)
+    create(:comment, mechanic: mechanic, user: user)
+
+    allow_any_instance_of(SessionsHelper).to receive(:current_user) { admin }
+    visit 'mechanics/1'
+
+    expect(page).to have_content 'example comment'
+    expect(page).to have_selector(:link_or_button, 'Delete')
+    click_on 'Delete'
+    expect(page).to have_content 'Comment successfully destroyed'
   end
 end
